d5ca8189a57d9448dcfdcae70c48f535d0f69d7a
stefanjuang <33438238+stefanjuang@users.noreply.github.com>
2018-03-23 22:15:17 -0400

    Add files via upload

--- dddash3.py
+++ dddash3.py
@@ -21,27 +21,23 @@ app_colors = {
     }
 
 X = deque(maxlen=20)
+X1 = deque(maxlen=20)
 Xi =[]
 
 Y0 = deque(maxlen=20)
 Y0.append(0)
-Yi0 =[]
 
 Y1 = deque(maxlen=20)
 Y1.append(0)
-Yi1 =[]
 
 Y2 = deque(maxlen=20)
 Y2.append(0)
-Yi2 =[]
 
 Y3 = deque(maxlen=20)
 Y3.append(0)
-Yi3 =[]
 
 Y4 = deque(maxlen=20)
 Y4.append(0)
-Yi4 =[]
 
 keyword=[]
 score=[]
@@ -67,58 +63,44 @@ app.layout = html.Div(
                                                   dcc.Input(id='sentiment_term', value=keyword[count], type='text', style={'color':app_colors['someothercolor']}),
                                                   ],
                  style={'width':'98%','margin-left':10,'margin-right':10,'max-width':50000}),
+        html.Div(className='row', children=[html.Div(dcc.Graph(id='live-table', animate=False), className='col s12 m6 l6')]),
         
-        html.Div(className='row', children=[html.Div(dcc.Graph(id='live-graph', animate=False), className='col s12 m6 l6')]),
+        html.Div(className='row', children=[html.Div(dcc.Graph(id='live-graph', animate=False), className='col s12 m6 l6'),
+                                                html.Div(dcc.Graph(id='live-donut', animate=False), className='col s12 m6 l6')]),
         
         dcc.Interval(
             id='graph-update',
-            interval=2*1000
+            interval=1.5*1000
         ),
-            ]
+        dcc.Interval(
+            id='table-update',
+            interval=1*1000
+        ),
+        dcc.Interval(
+            id='donut-update',
+            interval=1.5*1000
+        ),
+            ], style={'backgroundColor': app_colors['background'], 'height':'2000px',},
 )
 
+
+@app.callback(Output('live-table', 'figure'),
+              events=[Event('table-update', 'interval')])
+def update_table():
+    trace = go.Table(
+    header=dict(values=['Time', 'Tweets']),
+    cells=dict(values=[Xi,tweet]))
+    data1 = [trace] 
+    return{'data':data1}
+
+ 
 @app.callback(Output('live-graph', 'figure'),
               events=[Event('graph-update', 'interval')])
 
 def update_graph_scatter():
-    global count
-
-    if emotion[count] == 'fear':
-        Y0.append(Y0[-1]*0.8+float(score[count])*0.2)
-        Y1.append(Y1[-1]-Y1[-1]*0.1)
-        Y2.append(Y2[-1]-Y2[-1]*0.1)
-        Y3.append(Y3[-1]-Y3[-1]*0.1)
-        Y4.append(Y4[-1]-Y4[-1]*0.1)
-    elif emotion[count] == 'disgust':
-        Y1.append(Y1[-1]*0.8+float(score[count])*0.2)
-        Y0.append(Y0[-1]-Y0[-1]*0.1)
-        Y2.append(Y2[-1]-Y2[-1]*0.1)
-        Y3.append(Y3[-1]-Y3[-1]*0.1)
-        Y4.append(Y4[-1]-Y4[-1]*0.1)
-    elif emotion[count] == 'sadness':
-        Y2.append(Y2[-1]*0.8+float(score[count])*0.2)
-        Y1.append(Y1[-1]-Y1[-1]*0.1)
-        Y0.append(Y0[-1]-Y0[-1]*0.1)
-        Y3.append(Y3[-1]-Y3[-1]*0.1)
-        Y4.append(Y4[-1]-Y4[-1]*0.1)
-    elif emotion[count] == 'joy':
-        Y3.append(Y3[-1]*0.8+float(score[count])*0.2) 
-        Y1.append(Y1[-1]-Y1[-1]*0.1)
-        Y2.append(Y2[-1]-Y2[-1]*0.1)
-        Y0.append(Y0[-1]-Y0[-1]*0.1)
-        Y4.append(Y4[-1]-Y4[-1]*0.1)
-    elif emotion[count] == 'anger':
-        Y4.append(Y4[-1]*0.8+float(score[count])*0.2) 
-        Y1.append(Y1[-1]-Y1[-1]*0.1)
-        Y2.append(Y2[-1]-Y2[-1]*0.1)
-        Y3.append(Y3[-1]-Y3[-1]*0.1)
-        Y0.append(Y0[-1]-Y0[-1]*0.1)
-    else:
-        pass  
-
+    global count, Y0,Y1,Y2,Y3,Y4
     X.append(Xi[count])
     
-    count = count+1
     trace0 = go.Scatter(
             x=list(X),
             y=list(Y0),
@@ -150,11 +132,55 @@ def update_graph_scatter():
             name='anger',
             mode= 'lines+markers'
             )
+    if emotion[count] == 'fear':
+        Y0.append(Y0[-1]*0.95+float(score[count])*0.05)
+        Y1.append(Y1[-1]*0.95)
+        Y2.append(Y2[-1]*0.95)
+        Y3.append(Y3[-1]*0.95)
+        Y4.append(Y4[-1]*0.95)
+    elif emotion[count] == 'disgust':
+        Y1.append(Y1[-1]*0.95+float(score[count])*0.05)
+        Y0.append(Y0[-1]*0.95)
+        Y2.append(Y2[-1]*0.95)
+        Y3.append(Y3[-1]*0.95)
+        Y4.append(Y4[-1]*0.95)
+    elif emotion[count] == 'sadness':
+        Y2.append(Y2[-1]*0.95+float(score[count])*0.05)
+        Y1.append(Y1[-1]*0.95)
+        Y0.append(Y0[-1]*0.95)
+        Y3.append(Y3[-1]*0.95)
+        Y4.append(Y4[-1]*0.95)
+    elif emotion[count] == 'joy':
+        Y3.append(Y3[-1]*0.95+float(score[count])*0.05) 
+        Y1.append(Y1[-1]*0.95)
+        Y2.append(Y2[-1]*0.95)
+        Y0.append(Y0[-1]*0.95)
+        Y4.append(Y4[-1]*0.95)
+    elif emotion[count] == 'anger':
+        Y4.append(Y4[-1]*0.95+float(score[count])*0.05) 
+        Y1.append(Y1[-1]*0.95)
+        Y2.append(Y2[-1]*0.95)
+        Y3.append(Y3[-1]*0.95)
+        Y0.append(Y0[-1]*0.95)
+    else:
+        pass  
+
+    count = count+1
     
     data = [trace0, trace1, trace2, trace3, trace4]
-    return {'data': data,'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),yaxis=dict(range=[min(min(Y0),min(Y1),min(Y2),min(Y3),min(Y4))-0.01,max(max(Y0), max(Y1), max(Y2), max(Y3), max(Y4))+0.01]))}
+    return {'data': data,'layout' : go.Layout(title='Emotion Analysis for "{}" on Twitter'.format(keyword[count]),xaxis=dict(range=[min(X),max(X)]),yaxis=dict(range=[min(min(Y0),min(Y1),min(Y2),min(Y3),min(Y4))-0.01,max(max(Y0), max(Y1), max(Y2), max(Y3), max(Y4))+0.01]))}
 
+@app.callback(Output('live-donut', 'figure'),
+              events=[Event('donut-update', 'interval')])
+def update_donut():
+    labels = ['fear','disgust','sadness','joy','anger']
+    values = [abs(Y0[-2]),abs(Y1[-2]),abs(Y2[-2]),abs(Y3[-2]),abs(Y4[-2])]
+    colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']
 
+    tracepie = go.Pie(labels=labels, values=values,marker=dict(colors=colors, 
+                           line=dict(color='#000000', width=2)))
+    data2 = [tracepie] 
+    return{'data':data2}
 
 if __name__ == '__main__':
     app.run_server(debug=False)
